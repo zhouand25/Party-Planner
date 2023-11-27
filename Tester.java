d04ec945c7c4f6489870c60e7bafc8e1268b6a8f
zhouand25 <124077648+zhouand25@users.noreply.github.com>
2023-11-27 16:25:19 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,91 +1,41 @@
- import java.io.File;
- import java.io.FileNotFoundExcwp
- import java.util.Scanner;
- 
- public class Tester {
- 
-ArrayList<attendee> visitors = new ArrayList<attendee>();
-int[][] tables = new int[10][10];
-int[] tablelength = new int[10];
- 
-//Most of the fileio code directly copied from w3schools  
+import java.util.Scanner;
+/**
+ * Main.java
+ * @author Andrew Zhou
+ * @since 10/20/23
+ * @version 1.0.1
+ * This class is essentially the driver/tester class which basically is where the program is execueted and the direction of
+ * entire program is controlled. It also provides input for the text files that hold the guest files and company files and hold
+ * the configuration details and options for the tables and seats.
+ */
+
+
+class Tester {
+  /*
+  This class directs the program through four major stages: the first stage is the file input where the user inputs the guest
+  and company file, configures the details, and creates the party instance. The second stage is when the class calls the manual adjust method and the user 
+  themselves can add or register other individuals. The third stage is the arrangement where the program arranges the guests into
+  tables. Finally, the last stage is where the user can recieve feedback and is when the tester class calls the results method.
+  */
+  //Most of the fileio code directly copied from w3schools  
   public static void main(String[] args) {
-	  try {
-      File myObj = new File("partyguests.txt");
-      Scanner myReader = new Scanner(myObj);
-      ArrayList<attendee> tempVisit = new ArrayList<attendee>();
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        String[] temp = data.splice(",");
-		binaryInsert(temp[2], temp[1], temp[3]);
-      }
-      manualAdjust();
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-   }
-  
-   
-   public static void binaryInsert(String firstName, String lastName, String company) {
-	      int min = 0;
-	      int max = visitors.size()-1; 
-		  int counter = 0;
-		  
-		  while(Math.pow(2,counter)<visitor.size()) {
-			++counter;
-			int mid = (min+max)/2;
-			int value = parseInt(company);
-			
-			if(value == visitor.get(mid).getCompany) {
-			  visitor.add(value, new attendee(firstName, lastName, company));
-			  return;
-			}
-			if(value>visitor.get(mid).getCompany) {
-			  min = mid;		
-			}
-			if(value>visitor.get(mid).getCompany) {
-			  max = mid;		
-			}
-			
-		  }
-		  visitor.add(max, new attendee(firstName, lastName, company);    
-   }
-   
-   public static void manualAdjust() {
-	 //Maybe put in tester class and not this class which is technically party but too lazy to change
-	 System.out.println("Hello, Welcome to PARTY PLANNER!\n");
-	 while (true) {
-	   System.out.print("If you want to add more people, type 1. If you want to start the arrangement, type 0: ");
-	   Scanner r1 = new Scanner(System.in)
-	   int mode = r1.nextInt();
-	   if(mode==0) {
-		 break;   
-	   }
-	   if(mode==1) {
-		System.out.print("First Name: ");
-		Scanner s1 = new Scanner(System.in);
-		String first = s1.nextLine();
-		
-		System.out.print("Last Name: ");
-		Scanner s2 = new Scanner(System.in);
-		String last = s2.nextLine();
-		
-		//Assume its an int for now but needs to be changed
-		System.out.print("Company: ");
-        Scanner s3 = new Scanner(System.in);
-        int company = s3.nextInt();
-        
-		binaryInsert(first, last, company);
-		System.out.println("User Added\n");
-	   }
-	   arrangement();
-	 }  
+    System.out.println("Welcome to Party Planner!");
+    System.out.print("\n\nFile name containing party guests: ");
+    Scanner s1 = new Scanner(System.in);
+    String name1 = s1.nextLine();
+    System.out.print("\nFile name containing companies: ");
+    Scanner s2 = new Scanner(System.in);
+    String name2 = s2.nextLine();
+    System.out.print("\nNumber of tables: ");
+    Scanner s3 = new Scanner(System.in);
+    int name3 = s3.nextInt();
+    System.out.print("\nNumber of seats per table: ");
+    Scanner s4 = new Scanner(System.in);
+    int name4 = s4.nextInt();
+    //Creates the instance of a party and configures the details
+    Party event = new Party(name1, name2, name3, name4);
+    event.manualAdjust();
+    event.arrange();
+    event.results();
   }
-  
-  public static void arrangement() {
-	  
-  }
- 
- }
+}
